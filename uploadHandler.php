841223a81c82b4e276cfc111b65eff6fc61dd6c9
moonkop <moonkop@qq.com>
2021-02-13 01:46:08 +0800

    add file overwrite function

--- uploadHandler.php
+++ uploadHandler.php
@@ -9,18 +9,19 @@ function failure($code, $msg)
     exit();
 }
 
-function success($successFileNames)
+function success($successFileNames,$overWriteFileNames)
 {
     global $target_dir;
-    $returnUrls=array();
+    $returnUrls = array();
 
     foreach ($successFileNames as $item) {
-        $url = 'http://'.$_SERVER['HTTP_HOST'] .'/'. $target_dir.$item;
+        $url = 'http://' . $_SERVER['HTTP_HOST'] . '/' . $target_dir . $item;
         $returnUrls[] = $url;
     }
     echo json_encode(array('code' => '100',
-        'payload'=>array(
-            'urls' => $returnUrls
+        'payload' => array(
+            'urls' => $returnUrls,
+            'overwrites' => $overWriteFileNames,
         )
     ));
     exit();
@@ -30,13 +31,14 @@ function getRandomNum()
 {
     return rand(10000, 99999);
 }
+
 $target_dir = "upload/";
 function main()
 {
     $cover = $_REQUEST['cover'];
     $singleDir = $_REQUEST['singleDir'];
 
-  global $target_dir;
+    global $target_dir;
 
     if ($singleDir) {
         $target_dir .= time();
@@ -50,28 +52,53 @@ function main()
         }
     }
 
-    if (count($existFiles) != 0) {
-        failure('500', join(",", $existFiles) . '人家已经有了呢');
-    } else {
-        $successFiles = array();
-        $moveErrorNames = array();
-        for ($i = 0; $i < count($_FILES["fileToUpload"]['error']); $i++) {
-            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"][$i]);
-            if (!move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file)) {
-                $moveErrorNames[] = $_FILES["fileToUpload"]["name"][$i];
-            } else {
-                $successFiles[] = $_FILES["fileToUpload"]["name"][$i];
-            }
-        }
-
-        if (count($moveErrorNames) == 0) {
-            success($successFiles);
+    $successFiles = array();
+    $moveErrorNames = array();
+    for ($i = 0; $i < count($_FILES["fileToUpload"]['error']); $i++) {
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"][$i]);
+        if (!move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file)) {
+            $moveErrorNames[] = $_FILES["fileToUpload"]["name"][$i];
         } else {
-            failure('206', '有一部分成功进来辣，但是' . join(',', $moveErrorNames) . '失败了呢');
+            $successFiles[] = $_FILES["fileToUpload"]["name"][$i];
         }
+    }
+
+    if (count($moveErrorNames) == 0) {
+        success($successFiles,$existFiles);
+    } else {
+        failure('206', '有一部分成功进来辣，但是' . join(',', $moveErrorNames) . '失败了呢');
     }
 }
 
-main();
+function checkExist()
+{
+    $names = $_REQUEST['names'];
+    global $target_dir;
+    $existFiles = [];
+    for ($i = 0; $i < count($names); $i++) {
+        $target_file = $target_dir . $names[$i];
+        if (file_exists($target_file)) {
+            $existFiles[] = $names[$i];
+        }
+    }
+
+    echo json_encode([
+        'code' => 200,
+        'payload' => [
+            'exists' => $existFiles
+        ]
+    ]);
+}
+
+$action = $_GET['action'];
+switch ($action) {
+    case 'upload':
+        main();
+        break;
+    case 'checkExist':
+        checkExist();
+        break;
+
+}
 
 ?>
